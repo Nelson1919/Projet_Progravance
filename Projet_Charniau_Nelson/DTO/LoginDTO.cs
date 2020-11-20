35b223e21ca77ea82e4cfb81978b5c5e4b6b0c38
Nelson <nelson.charniau@condorcet.be>
2020-11-20 15:06:24 +0100

    Séparation admin/Client Refonte du menu

--- Projet_Charniau_Nelson/DTO/LoginDTO.cs
+++ Projet_Charniau_Nelson/DTO/LoginDTO.cs
@@ -10,7 +10,6 @@ namespace Projet_Charniau_Nelson.DTO
         public int id_user{ get; set; }
         public string name { get; set; }
         public string firstname { get; set; }
-
         public Boolean admin { get; set; }
     }
 }
